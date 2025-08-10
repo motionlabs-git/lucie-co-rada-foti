74d7dea18b2947a620013ce916d95ffb8da2264d
jindrahabarta <jindriskuv@email.cz>
2025-08-10 12:39:03 +0200

    loader change

--- src/app/(front)/sections/references/References.tsx
+++ src/app/(front)/sections/references/References.tsx
@@ -1,92 +1,84 @@
 import React from 'react'
-import useEmblaCarousel from 'embla-carousel-react';
-import AutoScroll from 'embla-carousel-auto-scroll';
-import ReferenceCard from '@/components/References/ReferenceCard';
+import useEmblaCarousel from 'embla-carousel-react'
+import AutoScroll from 'embla-carousel-auto-scroll'
+import ReferenceCard from '@/components/References/ReferenceCard'
 
-const fakeData = [{
-    title: 'title1',
-    description: 'alsdfjls asldjflasdjf asldjfl asdjlfjasd ',
-    image: 'https://www.brides.com/thmb/LMyiMPxRFx82BLiHZC8lySJFnGo=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/marriage-pose-photo-recirc-kyle-john-1-29-4f97523aa049471992292e8d6ddc41ee.jpg'
-}, {
-    title: 'title1',
-    description: 'alsdfjls asldjflasdjf asldjfl asdjlfjasd ',
-    image: 'https://www.brides.com/thmb/LMyiMPxRFx82BLiHZC8lySJFnGo=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/marriage-pose-photo-recirc-kyle-john-1-29-4f97523aa049471992292e8d6ddc41ee.jpg'
-}, {
-    title: 'title1',
-    description: 'alsdfjls asldjflasdjf asldjfl asdjlfjasd ',
-    image: 'https://www.brides.com/thmb/LMyiMPxRFx82BLiHZC8lySJFnGo=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/marriage-pose-photo-recirc-kyle-john-1-29-4f97523aa049471992292e8d6ddc41ee.jpg'
-}, {
-    title: 'title1',
-    description: 'alsdfjls asldjflasdjf asldjfl asdjlfjasd ',
-    image: 'https://www.brides.com/thmb/LMyiMPxRFx82BLiHZC8lySJFnGo=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/marriage-pose-photo-recirc-kyle-john-1-29-4f97523aa049471992292e8d6ddc41ee.jpg'
-}, {
-    title: 'title1',
-    description: 'alsdfjls asldjflasdjf asldjfl asdjlfjasd ',
-    image: 'https://www.brides.com/thmb/LMyiMPxRFx82BLiHZC8lySJFnGo=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/marriage-pose-photo-recirc-kyle-john-1-29-4f97523aa049471992292e8d6ddc41ee.jpg'
-}, {
-    title: 'title1',
-    description: 'alsdfjls asldjflasdjf asldjfl asdjlfjasd ',
-    image: 'https://www.brides.com/thmb/LMyiMPxRFx82BLiHZC8lySJFnGo=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/marriage-pose-photo-recirc-kyle-john-1-29-4f97523aa049471992292e8d6ddc41ee.jpg'
-}]
-
+const fakeData = [
+	{
+		title: 'title1',
+		description: 'alsdfjls asldjflasdjf asldjfl asdjlfjasd ',
+		image: 'https://www.brides.com/thmb/LMyiMPxRFx82BLiHZC8lySJFnGo=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/marriage-pose-photo-recirc-kyle-john-1-29-4f97523aa049471992292e8d6ddc41ee.jpg',
+	},
+	{
+		title: 'title1',
+		description: 'alsdfjls asldjflasdjf asldjfl asdjlfjasd ',
+		image: 'https://www.brides.com/thmb/LMyiMPxRFx82BLiHZC8lySJFnGo=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/marriage-pose-photo-recirc-kyle-john-1-29-4f97523aa049471992292e8d6ddc41ee.jpg',
+	},
+	{
+		title: 'title1',
+		description: 'alsdfjls asldjflasdjf asldjfl asdjlfjasd ',
+		image: 'https://www.brides.com/thmb/LMyiMPxRFx82BLiHZC8lySJFnGo=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/marriage-pose-photo-recirc-kyle-john-1-29-4f97523aa049471992292e8d6ddc41ee.jpg',
+	},
+	{
+		title: 'title1',
+		description: 'alsdfjls asldjflasdjf asldjfl asdjlfjasd ',
+		image: 'https://www.brides.com/thmb/LMyiMPxRFx82BLiHZC8lySJFnGo=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/marriage-pose-photo-recirc-kyle-john-1-29-4f97523aa049471992292e8d6ddc41ee.jpg',
+	},
+	{
+		title: 'title1',
+		description: 'alsdfjls asldjflasdjf asldjfl asdjlfjasd ',
+		image: 'https://www.brides.com/thmb/LMyiMPxRFx82BLiHZC8lySJFnGo=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/marriage-pose-photo-recirc-kyle-john-1-29-4f97523aa049471992292e8d6ddc41ee.jpg',
+	},
+	{
+		title: 'title1',
+		description: 'alsdfjls asldjflasdjf asldjfl asdjlfjasd ',
+		image: 'https://www.brides.com/thmb/LMyiMPxRFx82BLiHZC8lySJFnGo=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/marriage-pose-photo-recirc-kyle-john-1-29-4f97523aa049471992292e8d6ddc41ee.jpg',
+	},
+]
 
 const References = () => {
+	const [emblaRef] = useEmblaCarousel({ loop: true, dragFree: true }, [
+		AutoScroll({
+			speed: 1.2,
+			startDelay: 0,
+			direction: 'forward',
+			stopOnInteraction: false,
+			stopOnMouseEnter: true,
+		}),
+	])
+	let counter = 0
 
-    const [emblaRef] = useEmblaCarousel({ loop: true, dragFree: true }, [
-        AutoScroll({
-            speed: 1.2,
-            startDelay: 0,
-            direction: 'forward',
-            stopOnInteraction: false,
-            stopOnMouseEnter: true,
-        }),
-    ]);
-    let counter = 0
+	return (
+		<section className='w-full relative'>
+			<div>
+				<h1 className='text-black invertText text-center'>
+					<em className='text-6xl'>Reference</em>
+				</h1>
+			</div>
 
+			<div className='embla w-full relative py-10' ref={emblaRef}>
+				<div className='embla__container_references items-center'>
+					{fakeData.map((slide, i) => {
+						counter++
 
+						if (counter === 3) {
+							counter = 0
+						}
 
-    return (
-
-        <section className='w-full relative'>
-
-
-            <div >
-                <h1 className='text-black invertText text-center'><em className='text-6xl'>Reference</em></h1>
-            </div>
-
-            <div
-                className="embla w-full relative py-10"
-                ref={emblaRef}>
-
-                <div className="embla__container_references items-center">
-
-                    {fakeData.map((slide, i) => {
-
-
-                        counter++
-
-                        if (counter === 3) {
-                            counter = 0
-                        }
-
-                        return (
-                            <ReferenceCard
-                                key={i}
-                                title={slide.title}
-                                description={slide.description}
-                                image={slide.image}
-                                counter={counter}
-                            />
-                        )
-                    }
-
-
-                    )}
-                </div>
-
-            </div>
-        </section>
-    )
+						return (
+							<ReferenceCard
+								key={i}
+								title={slide.title}
+								description={slide.description}
+								image={slide.image}
+								counter={counter}
+							/>
+						)
+					})}
+				</div>
+			</div>
+		</section>
+	)
 }
 
 export default References
--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -4,51 +4,54 @@ import React, { RefObject, useEffect, useState } from 'react'
 import gsap from 'gsap'
 
 const Loader = ({ lenis }: { lenis: RefObject<Lenis | null> }) => {
+	const [isActive, setIsActive] = useState(true)
 
-    const [isActive, setIsActive] = useState(true)
+	useEffect(() => {
+		setTimeout(() => {
+			function raf(time: number) {
+				if (!lenis.current) return
 
+				lenis.current.raf(time)
+				requestAnimationFrame(raf)
+			}
+			requestAnimationFrame(raf)
 
-    useEffect(() => {
+			lenis.current?.scrollTo(0, {
+				immediate: true,
+				onComplete: () => {
+					gsap.to('#loader', {
+						opacity: 0,
+						duration: 0.5,
 
-        setTimeout(() => {
+						onComplete: () => {
+							setIsActive(false)
+						},
+					})
+				},
+			})
+		}, 200)
 
-            function raf(time: number) {
-                if (!lenis.current) return
+		// gsap.to()
+	}, [lenis])
 
-                lenis.current.raf(time)
-                requestAnimationFrame(raf)
-            }
-            requestAnimationFrame(raf)
+	if (isActive)
+		return (
+			<section
+				id='loader'
+				className='fixed top-0 left-0 w-full h-screen bg-bone z-50 flex items-center justify-center'
+			>
+				<h1 className='absolute top-4 left-4 text-2xl leading-0'>
+					<strong className='text-2xl'>Načítání...</strong>
+				</h1>
 
-            lenis.current?.scrollTo(0, {
-                immediate: true, onComplete: () => {
-                    gsap.to('#loader', {
-                        opacity: 0,
-                        duration: 0.5,
-
-                        onComplete: () => {
-                            setIsActive(false)
-                        }
-                    })
-                }
-            })
-        }, 200);
-
-        // gsap.to()
-
-    }, [lenis])
-
-
-
-    if (isActive)
-        return (
-            <section id="loader" className='fixed top-0 left-0 w-full h-screen bg-bone z-50 flex items-center justify-center'>
-                <h1 className='absolute top-4 left-4 text-2xl leading-0'><strong className='text-2xl'>Načítání...</strong></h1>
-
-                <h1 className='flex gap-4 items-baseline'><em className=''>Připravit</em><em className=''>Úsměr</em><em className=''>Sýr!</em></h1>
-                {/* <h1 className='flex gap-4 items-baseline'><strong>Vyfotím vaše</strong><em className='text-orange'>Svatby</em><em className='text-orange'>Akce</em><em className='text-orange'>Večírky</em></h1> */}
-            </section>
-        )
+				<h1 className='flex gap-4 items-baseline'>
+					<em className=''>Připravit</em>
+					<em className=''>Úsměr</em>
+					<em className=''>Sýr!</em>
+				</h1>
+				{/* <h1 className='flex gap-4 items-baseline'><strong>Vyfotím vaše</strong><em className='text-orange'>Svatby</em><em className='text-orange'>Akce</em><em className='text-orange'>Večírky</em></h1> */}
+			</section>
+		)
 }
 
 export default Loader
