c6ef9b25f8cbfaf3c87d97a0652cf3eef4fc26a2
jindrahabarta <jindriskuv@email.cz>
2025-10-07 19:00:43 +0200

    admin noindex

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,5 +1,12 @@
 import './admin.css'
 
+export const metadata = {
+	title: 'Lucie co ráda fotí | Administrace',
+	robots: {
+		index: false,
+		follow: false,
+	},
+}
 export default function AdminLayout({
 	children,
 }: Readonly<{
