921f291531e80f312a8ea27598afcf933a84e42c
jindrahabarta <jindriskuv@email.cz>
2026-03-26 10:39:45 +0100

    www.added

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,13 +3,13 @@ import type { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
 	return [
 		{
-			url: 'https://luciefoti.cz',
+			url: 'https://www.luciefoti.cz',
 			lastModified: new Date(),
 			changeFrequency: 'monthly',
 			priority: 1,
 		},
 		{
-			url: 'https://luciefoti.cz/gdpr',
+			url: 'https://www.luciefoti.cz/gdpr',
 			lastModified: new Date(),
 			changeFrequency: 'monthly',
 			priority: 0.1,
